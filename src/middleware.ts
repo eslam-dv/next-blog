870becf96427cb472303dd3495a21bc1708deb1d
Eslam Osama <noneofyourbusiness@email.com>
2025-10-14 10:31:25 +0300

    setup clerk webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/about", "/projects", "/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/about",
+  "/projects",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
