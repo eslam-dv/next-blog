c5f1c6ae34a916f1795dce2d15784737bf532682
Eslam Osama <noneofyourbusiness@email.com>
2025-10-16 11:03:52 +0300

    handle user deletion

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,4 +1,4 @@
-import { createUser, updateUser } from "@/lib/actions/user";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user";
 import { clerkClient } from "@clerk/nextjs/server";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
@@ -51,6 +51,12 @@ export async function POST(req: NextRequest) {
         });
       }
     }
+
+    if (eventType === "user.deleted") {
+      const { id } = evt?.data;
+      await deleteUser(id!);
+    }
+
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
     console.error("Error verifying webhook:", err);
